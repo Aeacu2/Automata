9e5ab94b77edf73dc4c30602a90900507c8e841f
Lean 4 VS Code Extension <>
2024-09-16 02:04:30 -0400

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "automata" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Automata» where
+  -- add any library configuration options here
