6a42abc4afd33b2047ebf5ccf0cdd69260ccc9a8
Aeacu2 <aeacusshen@gmail.com>
2024-10-06 02:52:20 -0400

    just commit

--- Automata/Common.lean
+++ Automata/Common.lean
@@ -55,7 +55,7 @@ def falseDFA: DFA (Fin 0) (Fin 1) := {
 #eval falseDFA.eval []
 
 
-def eqBase (n: ℕ) : DFA (Fin 2 → Fin n) (Fin 2) := {
+def eqBase (k: ℕ) : DFA (Fin 2 → Fin k) (Fin 2) := {
   transition := fun f s => match s with
     | 0 => if (f 0).val == f 1 then 0 else 1
     | 1 => 1
@@ -63,23 +63,34 @@ def eqBase (n: ℕ) : DFA (Fin 2 → Fin n) (Fin 2) := {
   output := fun x => x == 0
 }
 
+def eqBase' (k: ℕ) (a b n : ℕ) (ha: a < n) (hb: b < n): DFA (Fin n → Fin k) (Fin 2) := {
+  transition := fun f s => match s with
+    | 0 => if (f ⟨a, ha⟩).val == f ⟨b, hb⟩ then 0 else 1
+    | 1 => 1
+  start := 0
+  output := fun x => x == 0
+}
+
 #eval (eqBase 2).eval [fun _ => 0, fun _ => 1]
 #eval (eqBase 2).eval [fun x => x, fun x => x]
+#eval (eqBase' 2 0 1 2 (by norm_num) (by norm_num)).eval [fun _ => 0, fun _ => 1]
+#eval (eqBase' 2 0 1 2 (by norm_num) (by norm_num)).eval [fun x => x, fun x => x]
 
-def addBase (n: ℕ) : DFA (Fin 3 → Fin n) (Fin 3) := {
+
+def addBase (k: ℕ) : DFA (Fin 3 → Fin k) (Fin 3) := {
   transition := fun f s => match s with
     | 0 => if f 0 + f 1 == f 2 then 0 else
       if (f 0).val + f 1 + 1 == f 2 then 1
       else 2
-    | 1 => if (f 0).val + f 1 + 1 == f 2 + n then 1 else
-      if (f 0).val + f 1 == f 2 + n then 0
+    | 1 => if (f 0).val + f 1 + 1 == f 2 + k then 1 else
+      if (f 0).val + f 1 == f 2 + k then 0
       else 2
     | 2 => 2
   start := 0
   output := fun x => x == 0
 }
 
-def ltBase (n: ℕ)  : DFA (Fin 2 → Fin n) (Fin 3) := {
+def ltBase (k: ℕ)  : DFA (Fin 2 → Fin k) (Fin 3) := {
   transition := fun f s => match s with
     | 0 => if (f 0).val < (f 1).val then 1 else if (f 0).val == (f 1).val then 0 else 2
     | 1 => 1
@@ -95,17 +106,3 @@ def ltBase (n: ℕ)  : DFA (Fin 2 → Fin n) (Fin 3) := {
 def digits' (b: ℕ) (n: ℕ) (h: b > 1) : List (Fin b) :=
   (Nat.digits b n).attach.map (fun x => ⟨x.1, Nat.digits_lt_base h x.2⟩)
 #eval digits' 2 12 (by norm_num)
-
-
-/- Q: Number bases in lean?
-
- Q: How to project a variable that's not in the first component?
-
- Q: How to deal, generally with formulas containing subformulas
- that does not have all the variables:
- like ∀x, y, x + y = 1 ∧ (x = 0 ∨ y = 0)?
- Need to extend the alphabet type of automatas to accomondate?
-
- Or use product alphabet: then projection becomes trickier
-
--/
