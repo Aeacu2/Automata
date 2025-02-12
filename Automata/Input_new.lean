b04ab27a74af6edb955c943e856cedd9d444ba38
Aeacu2 <aeacusshen@gmail.com>
2025-02-12 12:21:48 -0500

    input_new

---
+++ Automata/Input_new.lean
@@ -0,0 +1,358 @@
+import Mathlib.Tactic
+import Mathlib.Data.List.Basic
+import Mathlib.Data.List.Lemmas
+import Mathlib.Data.Nat.Digits
+import Init.Data.List.Lemmas
+import Automata.Replicate
+import Automata.LeadingZeros
+
+/-
+Major function: inputToBase
+
+Idea: Given a vector of natural numbers 'l',
+
+Step 1: Convert each number to base b, get 'ls' : Fin m -> (List ℕ)
+
+Functions involved: toBase, mapToBase
+Theorems involved: digits_end_nonzero, toBase_lead_nonzero,
+toBase_lt_base, mapToBase_lt_base, mapToBase_length
+
+Step 2: Stretch each list to uniform length by adding leading zeros, get 'lss' : Fin m -> (List ℕ)
+
+Functions involved: addZeroes, stretchLen
+Theorems involved: addZeroes_elem, addZeroesLength, stretchLen_length, stretchLen_uniform
+
+Step 3: Zip the lists together, giving less than base proofs to turn Nats into Fin bs, to get the input to automatas: List (Fin (l.length) → Fin b)
+
+Functions involved: zipToAlphabetFin
+Theorems involved: zipTailHlb, zipTailHlss, zipTailHls, zipToAlphabetFin_length
+-/
+
+def toBase (b : ℕ) (n : ℕ): List ℕ :=
+  (Nat.digits b n).reverse
+
+theorem digits_end_nonzero (b: ℕ) (n: ℕ) (hb: b ≥ 2) (hn : n > 0) :
+  (Nat.digits b n)[(Nat.digits b n).length - 1]'(by
+  simp[Nat.digits]
+  split <;> try simp at hb
+  rename_i x y
+  rw[Nat.digitsAux.eq_def]
+  split <;> try simp at hn
+  simp
+  ) > 0 := by
+  have:= Nat.digits_of_two_le_of_pos hb hn
+  induction' n using Nat.strong_induction_on with n ih
+  simp[this]
+  have h1: n / b < n := Nat.div_lt_self hn hb
+  by_cases hnb : n / b ≠ 0
+  have: n / b > 0 := by exact Nat.zero_lt_of_ne_zero hnb
+  specialize ih (n / b) h1 this
+  have := Nat.digits_of_two_le_of_pos hb this
+  specialize ih this
+  aesop
+  . simp at hnb
+    have: b.digits (n / b) = [] := by
+      rw[hnb]
+      apply Nat.digits_zero
+    simp[this]
+    have: n = b * (n / b) + n % b := by
+      exact Eq.symm (Nat.div_add_mod n b)
+    simp[hnb] at this
+    rw[← this]
+    exact hn
+
+theorem toBase_len_nonzero (b: ℕ) (n: ℕ) (hb: b ≥ 2) (hn : n > 0) :
+  0 < (toBase b n).length := by
+  simp only [toBase, Nat.digits, List.length_reverse]
+  split <;> try simp at hb
+  rw[Nat.digitsAux.eq_def]
+  split <;> try simp at hn
+  simp
+
+theorem toBase_lead_nonzero (b: ℕ) (n: ℕ) (hb: b ≥ 2) (hn : n > 0) :
+  (toBase b n)[0]'(by apply toBase_len_nonzero<;> assumption) > 0 := by
+  simp[toBase]
+  exact digits_end_nonzero b n hb hn
+
+theorem toBase_lt_base (b: ℕ) (n: ℕ) (hb: b ≥ 2) :
+  x ∈ (toBase b n) → x < b := by
+  intro h
+  simp only [toBase] at h
+  have h1: x ∈ Nat.digits b n := List.mem_reverse.mp h
+  apply Nat.digits_lt_base
+  exact hb
+  exact h1
+
+def ofBase (b : ℕ) (l : List ℕ) : ℕ :=
+  l.foldl (fun x y => y + b * x) 0
+
+theorem ofBase_toBase (b: ℕ) (n: ℕ) : ofBase b (toBase b n) = n := by
+  simp only [toBase, ofBase, List.foldl_reverse, add_comm, mul_comm]
+  have h: Nat.ofDigits b (b.digits n) = n := Nat.ofDigits_digits b n
+  nth_rewrite 2 [← h]
+  rw [Nat.ofDigits_eq_foldr]
+  rfl
+
+theorem toBase_ofBase (b: ℕ) (l: List ℕ) (hb: 1 < b) (hlb : ∀ x ∈ l, x < b) (hlen : l.length > 0) (hlead : l[0] > 0) :
+  toBase b (ofBase b l) = l := by
+  have := Nat.digits_ofDigits b hb l.reverse
+  specialize this (by simp_all)
+  specialize this (by
+    intro h
+    simp[List.getLast]
+    suffices : ¬ l[0] = 0
+    . sorry
+    exact Nat.not_eq_zero_of_lt hlead
+  )
+  rw[toBase, ofBase]
+  rw[Nat.ofDigits_eq_foldr] at this
+  simp_all only [gt_iff_lt, Nat.cast_id, List.foldr_reverse, List.reverse_reverse]
+
+
+def addZeroes (n: ℕ) (l: List ℕ): List ℕ :=
+  (List.replicate n 0) ++ l
+
+theorem addZeroes_elem :
+  x ∈ (addZeroes n l) → x = 0 ∨ x ∈ l := by
+  intro h
+  simp only [addZeroes, List.mem_append, List.replicate] at h
+  rcases h with (h | h)
+  . left
+    have : x ∈ [0] := List.replicate_subset_singleton n 0 h
+    exact Finset.mem_zero.mp this
+  . right
+    exact h
+
+theorem addZeroesLength (n: ℕ) (l: List ℕ) :
+  (addZeroes n l).length = n + l.length :=
+  by simp only [addZeroes, List.length_append, List.length_replicate]
+
+theorem addZeroes_zero (n: ℕ) (l: List ℕ) (hi : i < n):
+  (addZeroes n l)[i]'(by simp[addZeroesLength]; omega) = 0 := by
+  simp only [addZeroes]
+  apply List.getElem_of_replicate_append_left
+  exact hi
+
+theorem ofBase_addZeroes (b: ℕ)(n: ℕ) (l: List ℕ) :
+  ofBase b (addZeroes n l) = ofBase b l := by
+  simp only [ofBase, addZeroes]
+  induction n
+  case zero => simp
+  case succ n ih =>
+    simp [addZeroes, List.replicate, List.foldl, ih]
+
+def maxLen : (l: List (List α)) → ℕ
+  | [] => 0
+  | x :: xs => max x.length (maxLen xs)
+
+theorem cons_lt_maxLen (l: List α) (ls: List (List α)) :
+  l.length ≤ maxLen (l :: ls) := by
+  induction ls generalizing l with
+  | nil => simp[maxLen]
+  | cons head tail _ =>
+    simp [maxLen]
+
+theorem len_le_maxLen (l: List α) (ls: List (List α)) :
+  l ∈ ls → l.length ≤ maxLen ls := by
+  intro h
+  induction ls generalizing l with
+  | nil => simp only [List.not_mem_nil] at h
+  | cons head tail ih =>
+    have h1: l = head ∨ l ∈ tail := List.eq_or_mem_of_mem_cons h
+    rcases h1 with (rfl | h1)
+    . apply cons_lt_maxLen
+    . simp[maxLen]
+      right
+      exact ih l h1
+
+def maxLenFin (v: (Fin m → List ℕ)) : ℕ :=
+  maxLen (List.ofFn v)
+
+theorem len_le_maxLenFin (v: (Fin m → List ℕ)) (i: Fin m) :
+  (v i).length ≤ maxLenFin v := by
+  apply len_le_maxLen
+  refine (List.mem_ofFn v (v i)).mpr ?_
+  exact Set.mem_range_self i
+
+def mapToBase (b: ℕ) (v : Fin m → ℕ) : Fin m → List ℕ :=
+  fun i => toBase b (v i)
+
+theorem mapToBase_lt_base (b: ℕ) (v : Fin m → ℕ) (hb: b > 1) (i : Fin m) :
+  ∀ x ∈ (mapToBase b v) i, x < b  := by
+  intro x h
+  simp only [mapToBase] at h
+  apply toBase_lt_base
+  omega
+  exact h
+
+def stretchLen (ls: Fin m → (List ℕ)) : Fin m → (List ℕ) :=
+  fun i => addZeroes (maxLenFin ls - (ls i).length) (ls i)
+
+theorem stretchLen_uniform (ls: Fin m → (List ℕ)) (i : Fin m) :
+  (stretchLen ls i).length = maxLenFin ls:= by
+  simp only [stretchLen, addZeroes, List.length_append, List.length_replicate]
+  have := len_le_maxLenFin ls i
+  omega
+
+theorem stretchLen_of_mapToBase_lt_base (b: ℕ) (v: Fin m → ℕ) (hb: b > 1) (i : Fin m) :
+  ∀ x ∈ (stretchLen (mapToBase b v)) i, x < b := by
+  intro x h
+  simp only [stretchLen, addZeroes] at h
+  have h1 := addZeroes_elem h
+  rcases h1 with (rfl | h1)
+  . omega
+  apply mapToBase_lt_base
+  . exact hb
+  . exact h1
+
+theorem zipTailHlb (ls: Fin m → (List ℕ)) (hlb: ∀ i, ∀ x ∈ ls i, x < b) :
+  ∀ i, ∀ x ∈ (ls i).tail, x < b := by
+  intro i x h
+  specialize hlb i
+  apply hlb
+  . apply List.mem_of_mem_tail h
+
+theorem zipTailHls (ls: Fin m → (List ℕ)) (hls : ∀ i, (ls i).length = l + 1) :
+  ∀ i, (ls i).tail.length = l := by
+  intro i
+  have h := List.length_tail (ls i)
+  rw[hls] at h
+  exact h
+
+def zip (ls: Fin m → (List ℕ)) (hlb: ∀ i, ∀ x ∈ ls i, x < (b + 2)) (hls : ∀ i, (ls i).length = l) : List (Fin m → Fin (b + 2))  :=
+  match l with
+  | 0 => []
+  | m+1 =>
+     (fun i =>
+       have : 0 < (ls i).length := by
+         rw[hls]
+         omega
+       ⟨(ls i)[0], by apply hlb; exact List.getElem_mem (ls i) 0 this⟩) :: (zip (fun i => (ls i).tail)
+        (by apply zipTailHlb; exact hlb)
+        (by apply zipTailHls; exact hls))
+
+def toWord (v: Fin m → ℕ) (b: ℕ) : List (Fin m → Fin (b + 2)) :=
+  zip (stretchLen (mapToBase (b + 2) v)) (by
+    apply stretchLen_of_mapToBase_lt_base
+    omega
+  ) (by
+    intro i
+    apply stretchLen_uniform
+  )
+
+def getDigits (ls : List (Fin m → Fin (b + 2))) : Fin m → List ℕ :=
+  fun i => List.map (fun f => f i) ls
+
+theorem getDigits_of_zip (ls: Fin m → (List ℕ)) (hlb: ∀ i, ∀ x ∈ ls i, x < (b + 2)) (hls : ∀ i, (ls i).length = (l + 1)) :
+  getDigits (zip ls hlb hls) = ls := by
+  funext i
+  simp only [getDigits, zip]
+  have h1: 0 < (ls i).length := by
+    rw[hls]
+    omega
+  have h2: (ls i).length = l + 1 := by
+    rw[hls]
+  simp only [List.map, zip]
+  induction l generalizing ls
+  case zero =>
+    have h3: (ls i).tail = [] := by
+        have : (ls i).tail.length = 0 := by
+          exact zipTailHls ls hls i
+        exact List.length_eq_zero.mp this
+    simp[zip]
+    exact Eq.symm (List.eq_of_length_one (ls i) (hls i))
+  case succ l ih =>
+    specialize ih (fun i => (ls i).tail)
+    specialize ih (by apply zipTailHlb; exact hlb)
+    specialize ih (by apply zipTailHls; exact hls)
+    specialize ih (by aesop) (by aesop)
+    suffices : List.map (fun f ↦ ↑(f i)) (zip (fun i ↦ (ls i).tail) _ _) = (ls i).tail
+    . rw[this]
+      have := @List.cons_head!_tail ℕ (by exact instInhabitedNat) (ls i) (by aesop)
+      sorry
+    simp only [List.map, zip]
+    exact ih
+
+theorem getDigits_length (ls : List (Fin m → Fin (b + 2))) (i : Fin m) : (getDigits ls i).length = ls.length := by
+  simp only [getDigits, List.length_map]
+
+def toNat_aux (b : ℕ) (Digits : Fin m → List ℕ) : Fin m → ℕ :=
+  fun i => ofBase (b + 2) (Digits i)
+
+theorem toNat_aux_of_mapToBase (b: ℕ) (v: Fin m → ℕ) :
+  toNat_aux b (mapToBase (b + 2) v) = v := by
+  funext i
+  simp[toNat_aux, mapToBase, ofBase_toBase]
+
+def toNat (ls : List (Fin m → Fin (b + 2))) : Fin m → ℕ :=
+  toNat_aux b (getDigits ls)
+
+theorem toWord_of_toNat (ls : List (Fin m → Fin (b + 2))) :
+  ∃ (k : ℕ), ls = padZeros k (toWord (toNat ls) b) := by
+  sorry
+
+theorem toNat_of_toWord (v: Fin m → ℕ) (b: ℕ) :
+  toNat (toWord v b) = v := by
+  simp[toNat, toNat_aux, getDigits, toWord, zip, stretchLen, addZeroes, mapToBase, toBase]
+  have : (getDigits (zip (stretchLen (mapToBase (b + 2) v)) (stretchLen_of_mapToBase_lt_base (b + 2) v (by omega)) (stretchLen_uniform (mapToBase (b + 2) v))) = mapToBase (b + 2) v) := by
+    -- apply getDigits_of_zip
+    sorry
+  rw[this]
+  exact toNat_aux_of_mapToBase b v
+
+/- USELESS CODES
+def digits' (b: ℕ) (n: ℕ) (h: b > 1) : List (Fin b) :=
+  (Nat.digits b n).attach.map (fun x => ⟨x.1, Nat.digits_lt_base h x.2⟩)
+
+def zipToAlphabet (n : ℕ) (l : ℕ) (lss: List (List ℕ)) (hlss: lss.length = l) (hls : ∀ ls ∈ lss, ls.length = n) : List (Fin l → ℕ) :=
+  match n with
+  | 0 => []
+  | m+1 =>
+     (fun i =>
+       have : 0 < lss[i].length := by
+         rw[hls]
+         omega
+         refine List.mem_iff_get.mpr ?_
+         subst hlss
+         use i
+         rfl
+
+       lss[i][0]) :: (zipToAlphabet m l (lss.map (fun ls => ls.tail))
+        (by simp only [List.length_map, hlss]) (fun
+          | .nil => by
+            simp only [List.mem_map, List.length_nil, forall_exists_index, and_imp]
+            intro x hx
+            specialize hls x hx
+            rcases x
+            . simp only [List.length_nil, self_eq_add_left, AddLeftCancelMonoid.add_eq_zero,
+              one_ne_zero, and_false] at hls
+            . simp only [List.length_cons, add_left_inj] at hls
+              simp only [List.tail_cons]
+              intro h
+              simp only [h, List.length_nil] at hls
+              assumption
+          | .cons head tail => by
+            simp only [List.mem_map, List.length_cons, forall_exists_index, and_imp]
+            intro x hx h
+            specialize hls x hx
+            have : x.tail.length = m := by
+              have: x.length - 1 = m := by
+                omega
+              rw[← this]
+              apply tailLen x
+            rw[← this, h]
+            have hxtail := tailLen x.tail
+            rw[h, List.tail] at hxtail
+            have: (head :: tail).length > 0 := by simp
+            omega
+            ))
+
+def toBaseZip (b : ℕ) (l: List ℕ) : (List (Fin l.length → ℕ)) :=
+  let ls := mapToBase b l
+  let n := maxLen ls
+  let lss := stretchLen (ls)
+  zipToAlphabet n l.length lss (by simp only [mapToBase_length b l, stretchLen_length ls]) (by
+    intro ls hls
+    apply stretchLen_uniform
+    assumption)
+-/
---
+++ Automata/Projection_new.lean
@@ -0,0 +1,276 @@
+import Mathlib.Tactic
+import Mathlib.Data.List.Basic
+import Batteries.Data.List.Basic
+import Mathlib.Data.Nat.Digits
+import Automata.DFA
+import Automata.NFA
+import Automata.Pumping
+import Automata.Input_new
+import Automata.LeadingZeros
+import Automata.Fin
+
+def project (m : Fin (n+1)) (dfa : DFA (Fin (n+1) → Fin (b+2)) state) [DecidableEq state] :
+  NFA (Fin n → Fin (b+2)) state := {
+  transition :=
+  fun a q => ⟨(List.map (fun (x : Fin (b+2)) => dfa.transition (recover_value m x a) q)
+    (FinEnum.toList (Fin (b+2)))).dedup, by apply List.nodup_dedup⟩
+  start := ⟨[dfa.start], List.nodup_singleton dfa.start⟩
+  output := dfa.output
+}
+
+-- theorem transition_project' [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (f : (Fin (n+1) → Fin (b+2))) (p q : state) : dfa.transition f p = q → q ∈ ((project m dfa).transition (remove_index m f) p).val := by
+--   simp only [project, List.mem_dedup, List.mem_map, FinEnum.mem_toList, true_and]
+--   intro h
+--   use f m
+--   simp[recover_value, insert_remove]
+--   exact h
+
+theorem transition_project [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (f : (Fin (n+1) → Fin (b+2))) (p : state) : dfa.transition f p ∈ ((project m dfa).transition (remove_index m f) p).val := by
+  simp only [project, List.mem_dedup, List.mem_map, FinEnum.mem_toList, true_and]
+  use f m
+  simp[recover_value, insert_remove]
+
+theorem transFrom_project [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin (n+1) → Fin (b+2))) (p q : state) : dfa.transFrom l p = q → q ∈ ((project m dfa).transFrom (l.map (fun f => remove_index m f)) ⟨[p], by exact List.nodup_singleton p⟩).val := by
+  intro h
+  induction l generalizing p
+  case nil =>
+    simp_all only [List.map_nil]
+    simp only [DFAO.transFrom] at h
+    simp only [NFA.transFrom, List.mem_singleton]
+    exact h.symm
+  case cons f fs ih =>
+    simp[DFAO.transFrom] at h
+    simp[NFA.transFrom, NFA.transList]
+    specialize ih (dfa.transition f p) h
+    have := NFA.transFrom_sublist (ps := ⟨[dfa.transition f p], by exact List.nodup_singleton (dfa.transition f p)⟩) (project m dfa) (List.map (fun f ↦ remove_index m f) fs) ⟨(((project m dfa).transition (remove_index m f) p).val).dedup, by apply List.nodup_dedup⟩
+    apply this
+    . simp only [List.cons_subset, List.mem_dedup, List.nil_subset, and_true]
+      apply transition_project
+    . assumption
+
+theorem evalFrom_project [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin (n+1) → Fin (b+2))) (p : state) : dfa.evalFrom l p → (project m dfa).evalFrom (l.map (fun f => remove_index m f)) ⟨[p], by exact List.nodup_singleton p⟩ := by
+  simp[NFA.evalFrom, DFAO.evalFrom]
+  intro h
+  use (DFAO.transFrom dfa l p)
+  constructor
+  . apply transFrom_project
+    rfl
+  . simp only [project]
+    exact h
+
+theorem eval_project [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin (n+1) → Fin (b+2))) : dfa.eval l → (project m dfa).eval (l.map (fun f => remove_index m f)) := by
+  simp[NFA.eval, DFAO.eval]
+  exact evalFrom_project dfa m l dfa.start
+
+-- theorem project_transition' [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (f : (Fin n → Fin (b+2))) (p q : state) : q ∈ ((project m dfa).transition f p).val → ∃ f₁, f = remove_index m f₁ ∧ dfa.transition f₁ p = q := by
+--   simp only [project, List.mem_dedup, List.mem_map, FinEnum.mem_toList, true_and]
+--   intro h
+--   rcases h with ⟨a, h'⟩
+--   use recover_value m a f
+--   constructor
+--   . simp only [recover_value, remove_insert]
+--   . exact h'
+
+theorem project_transition [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (f : (Fin n → Fin (b+2))) (p q : state) : q ∈ ((project m dfa).transition f p).val → ∃ a, dfa.transition (recover_value m a f) p = q := by
+  simp only [project, List.mem_dedup, List.mem_map, FinEnum.mem_toList, true_and, imp_self]
+
+theorem project_transFrom [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin n → Fin (b+2))) (q : state) (states : ListND state) : q ∈ ((project m dfa).transFrom l states).val → ∃ p ∈ states.val, ∃ (l₁ : List (Fin (n+1) → Fin (b+2))) , l = l₁.map (remove_index m) ∧ dfa.transFrom l₁ p = q:= by
+  intro h
+  induction l generalizing states
+  case nil =>
+    simp only [NFA.transFrom, List.mem_singleton] at h
+    simp_all only [List.nil_eq, List.map_eq_nil, exists_eq_left]
+    use q
+    simp only [h, DFAO.transFrom, and_self]
+  case cons f fs ih =>
+    simp [NFA.transFrom] at h
+    specialize ih ((project m dfa).transList f states)
+    specialize ih h
+    rcases ih with ⟨p, h₁, l₁, h₂, h₃⟩
+    -- Need to obtain a p' ∈ states.val from h₁. so p ∈ NFA.transList implies exists p', p  is in transitions.
+    have h₄ := NFA.transList_backtrack (project m dfa) f p states h₁
+    rcases h₄ with ⟨p', h₅, h₆⟩
+    use p'
+    constructor
+    . exact h₅
+    . have h₇ := project_transition dfa m f p' p h₆
+      rcases h₇ with ⟨a, h₈⟩
+      use (recover_value m a f) :: l₁
+      constructor
+      . simp only [recover_value, List.map_cons, remove_insert, List.cons.injEq, true_and]
+        simp only [h₂, remove_index]
+      . aesop
+
+theorem project_evalFrom [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin n → Fin (b+2))) (states : ListND state) : (project m dfa).evalFrom l states → ∃ p ∈ states.val, ∃ (l₁ : List (Fin (n+1) → Fin (b+2))) , l = l₁.map (remove_index m) ∧ dfa.evalFrom l₁ p:= by
+  intro h
+  simp_all [NFA.evalFrom, DFAO.evalFrom]
+  rcases h with ⟨p, h₁, h₂⟩
+  have h₃ := project_transFrom dfa m l p states h₁
+  rcases h₃ with ⟨p', h₄, h₅, h₆, h₇⟩
+  use p'
+  constructor
+  . exact h₄
+  . use h₅
+    constructor
+    . exact h₆
+    . simp only [project] at h₂
+      simp only [h₇, h₂]
+
+theorem project_eval [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin n → Fin (b+2))) : (project m dfa).eval l → ∃ (l₁ : List (Fin (n+1) → Fin (b+2))) , l = l₁.map (remove_index m) ∧ dfa.eval l₁ := by
+  simp[NFA.eval, DFAO.eval]
+  have h := project_evalFrom dfa m l (project m dfa).start
+  intro h₁
+  specialize h h₁
+  rcases h with ⟨p, h₂, l', h₄⟩
+  simp[project] at h₂
+  aesop
+
+theorem project_eval_iff [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (l : List (Fin n → Fin (b+2))) : (project m dfa).eval l ↔ ∃ (l₁ : List (Fin (n+1) → Fin (b+2))) , l = l₁.map (remove_index m) ∧ dfa.eval l₁ := by
+  constructor
+  . exact project_eval dfa m l
+  . intro h
+    rcases h with ⟨l₁, h₁, h₂⟩
+    have h₃ := eval_project dfa m l₁ h₂
+    aesop
+
+
+theorem project_acceptZero [DecidableEq state] (dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (h: DFA.acceptZero dfa) (m : Fin (n + 1)) : NFA.acceptZero (project m dfa) := by
+  simp[NFA.acceptZero]
+  intro l h'
+  have h₁ := project_eval_iff dfa m l
+  have h₂ := h₁.mp h'
+  rcases h₂ with ⟨l₁, h₃, h₄⟩
+  rw[DFA.acceptZero] at h
+  specialize h l₁ h₄
+  intro k
+  specialize h k
+  have h₅ := project_eval_iff dfa m (padZeros k l)
+  apply h₅.mpr
+  use (padZeros k l₁)
+  constructor
+  . simp only [padZeros, List.map_append, List.map_replicate]
+    have: List.replicate k (remove_index m (fun _ ↦ 0 : Fin (n+1) → Fin (b+2))) ++ List.map (remove_index m) l₁ = List.replicate k (fun _ ↦ 0) ++ List.map (remove_index m) l₁ := by
+      simp only [List.append_cancel_right_eq, List.replicate_inj, true_and]
+      right
+      simp only [remove_index]
+      ext x
+      simp only [Fin.removeNth, Fin.val_zero]
+    simp only [h₃, this]
+  . exact h
+
+theorem project_fix_acceptZero [Fintype state][DecidableEq state] (dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (h: dfa.acceptZero) : (project m dfa).fixLeadingZeros.acceptZero := by
+  have: (project m dfa).acceptZero := project_acceptZero dfa h m
+  exact NFA.fixLeadingZeroes.acceptZero (project m dfa) this
+
+theorem project_fix_respectZero [Fintype state][DecidableEq state] (dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (h: dfa.respectZero) : (project m dfa).fixLeadingZeros.respectZero := by
+  rw[NFA.respectZero]
+  intro l k
+  constructor
+  . have := project_fix_acceptZero dfa
+    have dfa_zero := dfa.acceptZero_of_respectZero h
+    have := this m dfa_zero
+    rw[NFA.acceptZero] at this
+    intro a
+    simp_all only [true_implies]
+  . intro h'
+    rw[NFA.fixLeadingZeros_eval, padZeros_add] at h'
+    have dfa_zero := dfa.acceptZero_of_respectZero h
+    have := project_acceptZero dfa dfa_zero m
+    have := (project m dfa).bounded_accept l this (padZeros (Fintype.card (ListND state) + k) l)
+    rw[NFA.fixLeadingZeros_eval]
+    apply this
+    constructor
+    . use (Fintype.card (ListND state) + k)
+    . exact h'
+
+theorem list_project_aux (x b : ℕ) (v : Fin m → ℕ) (i : Fin (m + 1)) : toNat (List.map (fun f ↦ remove_index i f) (toWord (recover_value i x v) b)) = v := by
+  sorry
+
+theorem list_project (x b : ℕ)  (v : Fin m → ℕ) (i : Fin (m + 1)) : ∃ k, (List.map (fun f ↦ remove_index m f) (inputToBase (b + 2) (by norm_num) (List.insertNth m x l) (by
+      simp_rw[← hl]
+      apply List.length_insertNth
+      omega
+    ))) = padZeros k (inputToBase (b+2) (by norm_num) l (by omega)) := by
+
+  have := inputToBase_of_reverseInput (List.map (fun f ↦ remove_index m f) (inputToBase (b + 2) (by norm_num) (List.insertNth m x l) (by
+      simp_rw[← hl]
+      apply List.length_insertNth
+      omega
+    )))
+  rcases this with ⟨k, h⟩
+  use k
+  rw[h]
+  congr
+  exact list_project_aux x b l hl m
+
+theorem correct_project [Fintype state] [DecidableEq state] (l : List ℕ) (hl : l.length = len)(m : Fin (len + 1)) (dfa : DFA (Fin (len+1) → Fin (b+2)) state) (hres: dfa.respectZero):
+  (∃ (x : ℕ), dfa.eval (inputToBase (b+2) (by norm_num) (l.insertNth m x) (by simp_rw[← hl]; apply List.length_insertNth; omega))) → (project m dfa).fixLeadingZeros.eval (inputToBase (b+2) (by omega) l hl):= by
+  rw[NFA.fixLeadingZeros_eval]
+  intro h
+  rcases h with ⟨x, h⟩
+  have := eval_project dfa m (inputToBase (b+2) (by norm_num) (l.insertNth m x) (by simp_rw[← hl]; apply List.length_insertNth; omega)) h
+  have lis := list_project x b l hl m
+  rcases lis with ⟨k, h₁⟩
+  rw[h₁] at this
+  apply (project m dfa).bounded_accept _
+  . apply project_acceptZero
+    exact DFA.acceptZero_of_respectZero dfa hres
+  swap
+  . exact padZeros k (inputToBase (b+2) (by norm_num) l (by omega))
+  . constructor
+    . use k
+    . exact this
+
+theorem project_correct [Fintype state] [DecidableEq state] (l : List ℕ) (hl : l.length = len)(m : Fin (len + 1)) (dfa : DFA (Fin (len+1) → Fin (b+2)) state) (hres: dfa.respectZero):
+  (project m dfa).fixLeadingZeros.eval (inputToBase (b+2) (by omega) l hl) → (∃ (x : ℕ), dfa.eval (inputToBase (b+2) (by norm_num) (l.insertNth m x) (by simp_rw[← hl]; apply List.length_insertNth; omega))):= by
+
+  intro h
+  rw[NFA.fixLeadingZeros_eval] at h
+  rw[project_eval_iff] at h
+  rcases h with ⟨l₁, hlis, hdfa⟩
+  have : dfa.eval (inputToBase (b + 2) (by norm_num) (reverseInput l₁) (by exact reverseInput_length l₁)) := by
+    have := inputToBase_of_reverseInput l₁
+    rcases this with ⟨k, h⟩
+    rw[h] at hdfa
+    rw[DFA.respectZero] at hres
+
+    specialize hres (inputToBase (b + 2) (by norm_num) (reverseInput l₁) (by exact reverseInput_length l₁)) k
+    apply hres.mpr
+    exact hdfa
+  use (reverseInput l₁)[m]'(by have := reverseInput_length l₁; omega)
+  have h₁ : (List.insertNth (↑m) ((reverseInput l₁)[m]'(by have := reverseInput_length l₁; omega)) l) = reverseInput l₁ := by
+
+    sorry
+  simp_rw[h₁]
+  exact this
+
+
+
+
+-- Legacy
+
+-- Auxilliary theorems for recover
+-- theorem finLt (m : Fin (n+1)) (b : ℕ) : b < m.val → b < n := by omega
+
+-- theorem finPred (m : Fin n) (a : Fin n) (h : a > m): a.val - 1 < n - 1 := by omega
+
+-- -- Auxilliary function for project
+-- def recover_value (m : Fin (n + 1)) (x: Fin (b+2)) (f: Fin n → Fin (b+2)) :
+--  (Fin (n + 1) → Fin (b+2)) :=
+--     fun i => if h1: i.val < m.val then f ⟨i.val, finLt m i.val h1⟩
+--       else if h2: i.val > m.val then f ⟨i.val - 1, finPred m i h2⟩ else x
+
+-- theorem project_transStep [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (x : Fin (b+2)) (f : (Fin n → Fin (b+2))) (p q : state) : dfa.transFrom [(recover_value m x f)] p = q → q ∈ ((project m dfa).transFrom [f] ⟨[p], by exact List.nodup_singleton p⟩).val := by
+--   simp only [DFAO.transFrom, NFA.transFrom, NFA.transList, project, List.bind_cons, List.bind_nil,
+--     List.append_nil, List.dedup_idem, List.mem_dedup, List.mem_map, FinEnum.mem_toList, true_and]
+--   intro a
+--   subst a
+--   simp_all only [exists_apply_eq_apply]
+
+-- theorem project_transStep [DecidableEq state](dfa : DFA (Fin (n + 1) → Fin (b+2)) state) (m : Fin (n + 1)) (f : (Fin (n+1) → Fin (b+2))) (p q : state) : dfa.transFrom [f] p = q → q ∈ ((project m dfa).transFrom [remove_index m f] ⟨[p], by exact List.nodup_singleton p⟩).val := by
+--   simp only [DFAO.transFrom, NFA.transFrom, NFA.transList, project, List.bind_cons, List.bind_nil,
+--     List.append_nil, List.dedup_idem, List.mem_dedup, List.mem_map, FinEnum.mem_toList, true_and]
+--   intro h
+--   use f m
+--   simp[recover_value, insert_remove]
+--   exact h
